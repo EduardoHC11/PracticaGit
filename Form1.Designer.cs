3b7b8fa8f3be24f12c76cca29100a1a9f5a6fbf5
Eduardo <edu.herrera088@gmail.com>
2025-09-11 10:54:01 -0500

    Agregue un label con el nombre: Resultado

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,22 +29,97 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.txtnum1 = new System.Windows.Forms.TextBox();
+            this.txtnum2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.txtresultado = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(353, 47);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(105, 24);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(307, 29);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "SUMA DE DOS NUMEROS";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtnum1
+            // 
+            this.txtnum1.Location = new System.Drawing.Point(217, 151);
+            this.txtnum1.Name = "txtnum1";
+            this.txtnum1.Size = new System.Drawing.Size(100, 20);
+            this.txtnum1.TabIndex = 1;
+            // 
+            // txtnum2
+            // 
+            this.txtnum2.Location = new System.Drawing.Point(217, 257);
+            this.txtnum2.Name = "txtnum2";
+            this.txtnum2.Size = new System.Drawing.Size(100, 20);
+            this.txtnum2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(145, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Numero 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(145, 264);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Numero 2";
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Location = new System.Drawing.Point(302, 372);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(81, 32);
+            this.btncalcular.TabIndex = 5;
+            this.btncalcular.Text = "Calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // txtresultado
+            // 
+            this.txtresultado.Location = new System.Drawing.Point(401, 379);
+            this.txtresultado.Name = "txtresultado";
+            this.txtresultado.Size = new System.Drawing.Size(100, 20);
+            this.txtresultado.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(398, 363);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Resultado";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(513, 430);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtresultado);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtnum2);
+            this.Controls.Add(this.txtnum1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +131,13 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtnum1;
+        private System.Windows.Forms.TextBox txtnum2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.TextBox txtresultado;
+        private System.Windows.Forms.Label label4;
     }
 }
 
